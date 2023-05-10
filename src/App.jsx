a3acf843de724c470341c2971320fc12e54aa0bc
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-10 21:52:11 +0600

    add coffee included

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className='text-center text-4xl mt-20'>Add new Coffee</h1>
+      <p className='text-center w-[60%] mx-auto mt-5'>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
+
+      <div className='w-full mx-auto mt-5'>
+
+        <form >
+        <div className='w-[50%] mx-auto grid grid-cols-2 gap-8'>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <label className="input-group">
+          
+            <input type="text" placeholder="Enter coffee name" className="input input-bordered" />
+          </label>
+          </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Chef</span>
+          </label>
+          <label className="input-group">
+          
+            <input type="text" placeholder="Enter coffee chef" className="input input-bordered" />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Supplier</span>
+          </label>
+          <label className="input-group">
+          
+            <input type="text" placeholder="Enter coffee Supplier" className="input input-bordered" />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Taste</span>
+          </label>
+          <label className="input-group">
+          
+            <input type="text" placeholder="Enter coffee Taste" className="input input-bordered" />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Details</span>
+          </label>
+          <label className="input-group">
+          
+            <input type="text" placeholder="Enter coffee Details" className="input input-bordered" />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Category</span>
+          </label>
+          <label className="input-group">
+          
+            <input type="text" placeholder="Enter coffee Category" className="input input-bordered" />
+          </label>
+        </div>
+        </div>
+        <div className=" w-[50%] mx-auto">
+          <label className="label">
+            <span className="">Photo URL</span>
+          </label>
+          <label >
+          
+            <input  type="text" placeholder="Enter coffee Photo URL" className="input input-bordered w-full" />
+          </label>
+        </div>
+        <div className=" w-[50%] mx-auto">
+          <button className='w-full btn btn-warning my-5'>Add Coffee</button>
+        </div>
+        </form>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+    
   )
 }
 
